c12fc1213dfdf13ce9310aed6d55a78c9c4f1640
WThawTar <145036789+WThawTar@users.noreply.github.com>
2023-09-14 21:41:19 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php include('header.php');
+include ('carousel.php') ?>
+<section class="bgactivity py-4">
+<div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+            <h2>Explore Activities</h2>
+      </div>  
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-3 mb-3">
+           <div class="border shadow text-center bg-white rounded p-2">
+                <img src="images/item15.jpg" class="w-100" alt="images">
+                <h4>Mountain Biking</h4>
+                <h6 class="fw-lighter">Mountain biking is a great way to explore surrounding your campsite. Find out the best routes through the scenic views.</h6>
+                <a href="service.php" class="btn w-50 btn-sm mb-3 btncolor">See More</a>
+           </div>
+        </div>
+        <div class="col-12 col-md-3 mb-3">
+           <div class="border shadow text-center bg-white rounded p-2">
+                <img src="images/item16.jpg" class="w-100" alt="images">
+                <h4>UnderWater Diving</h4>
+                <h6 class="fw-lighter">Divers can explore the underwater world and allows them to capture the beauty of marine life.</h6>
+                <a href="service.php" class="btn w-50 btn-sm mb-3 btncolor">See More</a>
+           </div>
+        </div>
+        <div class="col-12 col-md-3 mb-3">
+           <div class="border shadow text-center bg-white rounded p-2">
+                <img src="images/item17.jpg" class="w-100" alt="images">
+                <h4>Fishing</h4>
+                <h6 class="fw-lighter">Fishing is a popular activity that allows to connect with nature and enjoy the thrill of catching fish.</h6>
+                <a href="service.php" class="btn w-50 btn-sm mb-3 btncolor">See More</a>
+           </div>
+        </div>
+        <div class="col-12 col-md-3 mb-3">
+           <div class="border shadow text-center bg-white rounded p-2">
+                <img src="images/item18.jpg" class="w-100" alt="images">
+                <h4>Kayak</h4>
+                <h6 class="fw-lighter">Use your kayak to explore nearby lakes, rivers, or even small waterways surround you!</h6>
+                <a href="service.php" class="btn w-50 btn-sm mb-3 btncolor">See More</a>
+           </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<section class="py-4 indexbg">
+<div class="container">
+    <div class="row">
+        <div class="col-md-7 mt-5">
+            <h2>Accomodation and Provide Place to Stay</h2>
+            <p class="text-center fw-lighter">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum ipsa sapiente magni eos quibusdam corrupti obcaecati, commodi odit vel unde
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus modi quaerat quasi perferendis, sapiente error! Optio vitae nemo eaque impedit excepturi eos totam dolore magnam
+                 iure voluptatem tempora, nam quos. numquam velit provident, pariatur quod est reiciendis delectus assumenda explicabo.</p>
+        </div>
+        <div class="col-md-2">
+                <div class="border rounded shadow p-1 mt-4 text-center w-100" >
+                        <i class="bi bi-wifi fs"></i> <br>
+                        <span class="text-primary">Free Wifi</span>
+                </div>
+                <div class="border rounded shadow p-1 mt-4 text-center w-100" >
+                        <i class="bi bi-p-circle index"></i> <br>
+                        <span class="text-primary">Parking</span>
+                </div>
+                <div class="border rounded shadow p-1 mt-4 text-center w-100" >
+                        <i class="bi bi-bicycle index"></i> <br>
+                        <span class="text-primary">Free Bicycle</span>
+                </div>
+                    
+        </div>
+        <div class="col-md-3 mt-5">
+            <p>Abuptate mollitia dolor consectetur repellat hic sit ipsa, voluptatibus recusandae.</p>
+            <p class="mt-5">Abuptate mollitia dolor consectetur repellat hic sit ipsa, voluptatibus recusandae.</p>
+            <p class="mt-5">Abuptate mollitia dolor consectetur repellat hic sit ipsa, voluptatibus recusandae.</p>
+
+        </div>
+        
+        
+</div>
+</section>
+
+
+<?php include('footer.php');?> 
